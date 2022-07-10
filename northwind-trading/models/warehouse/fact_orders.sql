297c02a09974d42c5354d11a4f76b4438a912520
Tarek Lel <tareklel@gmail.com>
2022-07-10 09:29:59 +0200

    modified fact_orders

--- northwind-trading/models/warehouse/fact_orders.sql
+++ northwind-trading/models/warehouse/fact_orders.sql
@@ -1,17 +1,33 @@
+WITH
+source
+AS
+(
+  SELECT
+    id	as order_id,
+    employee_id,
+    customer_id,
+    order_date,
+    shipped_date,
+    shipper_id,
+    ship_name,
+    ship_address,
+    ship_city,
+    ship_state_province,
+    ship_zip_postal_code,
+    ship_country_region,
+    shipping_fee,
+    taxes
+  FROM
+    {{ ref('stg_orders') }}
+),
+unique_source AS
+(
+  SELECT *, ROW_NUMBER() OVER(PARTITION BY order_id, employee_id, customer_id) as _row
+  FROM source
+)
+
 SELECT
-  id	as order_id,
-  employee_id,
-  customer_id,
-  order_date,
-  shipped_date,
-  shipper_id,
-  ship_name,
-  ship_address,
-  ship_city,
-  ship_state_province,
-  ship_zip_postal_code,
-  ship_country_region,
-  shipping_fee,
-  taxes
+  * EXCEPT (_row)
 FROM
-  {{ ref('stg_orders') }}
+ unique_source
+WHERE _row = 1
